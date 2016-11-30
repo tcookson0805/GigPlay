c8cb7574ae8cd6573553e21a67a41ca000c6bf7a
Tyler Cookson <tcookson0805@gmail.com>
2016-11-30 10:59:59 -0800

    connected concertsReducer to rootReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux'
 
 import authReducer from './reducer-auth';
-// import concertReducer from './reducer-concerts';
+import concertsReducer from './reducer-concerts';
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  concerts: concertsReducer,
   routing: routerReducer
 });
 
