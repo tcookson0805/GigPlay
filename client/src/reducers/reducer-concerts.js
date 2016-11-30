55479b3679a1dd40e36129a8e67a99a0016fcb54
Tyler Cookson <tcookson0805@gmail.com>
2016-11-30 10:49:15 -0800

    initial creation of concert reducer

---
+++ client/src/reducers/reducer-concerts.js
@@ -0,0 +1,25 @@
+// import {
+//   FETCH_CONCERTS
+// } from '../actions/actions';
+
+// /** The initial state; no tokens and no user info */
+// const initialState = {
+
+// };
+
+// /**
+//  * Our reducer
+//  */
+// function concertReducer (state = initialState, action) {
+//   switch (action.type) {
+//   // when we get the tokens... set the tokens!
+//   case FETCH_CONCERTS:
+//     console.log(action.data);
+//     return Object.assign({}, state, action.data);
+
+//   default:
+//     return state;
+//   }
+// }
+
+// export default authReducer;
