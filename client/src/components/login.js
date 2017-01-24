929ca0d887f44943b91b2aefcc155ffc551430a7
Tyler Cookson <tcookson0805@gmail.com>
2017-01-23 18:44:54 -0800

    imported and used different loginSVG

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import loginSVG from '../log_in.svg';
 import BackgroundVideo from 'react-background-video';
 
+import loginSVG2 from '../../../public/9a1de5e2bdf092f25e0b62818dedd02e.svg'
 
 export default class Login extends Component {
   
@@ -23,6 +24,7 @@ export default class Login extends Component {
             <video src="../../style/videos/Cheer-Up/MP4/Cheer-Up.mp4" autoPlay loop></video>
           </div>
         </div>
+
         <div className="row">
           <div className="col-md-4">
           </div>
@@ -33,7 +35,7 @@ export default class Login extends Component {
                 <p className="login-hero-info-text1">Find concerts in your area <br />based on the songs you <br />listen to most.</p>
                 <p>And support artists you love</p>
                 <span className="login-hero-info-link">
-                  <a href="/login" dangerouslySetInnerHTML={{__html: loginSVG}}></a>                
+                  <a href="/login" dangerouslySetInnerHTML={{__html: loginSVG2}}></a>                
                 </span>
               </div>
             </div>
