784ac7ee8c2de6326e58de797bff41feedda49ee
Tyler Cookson <tcookson0805@gmail.com>
2017-01-23 22:58:49 -0800

    altered scss loader include directory

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
     loaders: [
       { test: /\.svg$/, loaders: ['raw-loader']},
       { test: /\.js?$/, loader: 'babel', exclude: /node_modules/ },
-      { test: /\.scss?$/, loader: 'style!css!sass', include: path.join(__dirname, 'src', 'styles') },
+      { test: /\.scss?$/, loader: 'style!css!sass', include: path.join(__dirname, 'client', 'src', 'styles') },
       { test: /\.png$/, loader: 'file' },
       { test: /\.css$/, loader: 'style-loader!css-loader', },
       { test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9]+)?$/, loader: 'file'}
