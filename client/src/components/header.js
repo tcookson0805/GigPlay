e0841e6b9f6d3665eb511eacdda6768244fa63d5
Tyler Cookson <tcookson0805@gmail.com>
2017-01-30 14:59:52 -0800

    incorporated fallback image if user doesnt have spotify profile pic

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -29,9 +29,29 @@ class Header extends Component {
     // console.log('HEADER ------ userInfo', userInfo)
     
     if(!userInfo){
-      return <div></div>
+      return (
+        <div className="header col-md-12">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="header-logo">
+                <img src="../../style/images/Gigplay-logo-1x.png" alt="Gigplay Logo"/>
+              </div>
+              <div className="header-user">
+                
+                <div className="header-user-pic">
+                  <img src="../../style/images/Spotify_Icon_RGB_Green.png" alt="" className="header-user-pic-spotify" />
+                  <img src="../../style/images/user.png" alt="" className="header-user-pic-image" />
+                </div>
+                
+                <div className="header-user-dropdown">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
     } else {
-      const image = userInfo.images ? userInfo.images[0].url : '../../style/images/ticket2.png';
+      const image = userInfo.images ? userInfo.images[0].url : '../../style/images/user.png';
       const display_name = userInfo.display_name ? userInfo.display_name : userInfo.id;
       return (
         <div className="header col-md-12">
