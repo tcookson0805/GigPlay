83412092b77100bd4d60a0b3757d35537bd58b03
Tyler Cookson <tcookson0805@gmail.com>
2017-01-31 15:00:12 -0800

    added info for demo account

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -27,12 +27,26 @@ export default class Login extends Component {
           <div className="col-md-4 offset-md-4">
             <div className="login-hero">
               <div className="login-hero-info">
-                <img src="../../style/images/Gigplay-logo-1x.png" alt="Gigplay Logo"/>
-                <p className="login-hero-info-text1">Find concerts in your area <br />based on the songs you <br />listen to most.</p>
-                <p>And support artists you love</p>
-                <span className="login-hero-info-link">
+                <img class="login-hero-logo" src="../../style/images/Gigplay-logo-1x.png" alt="Gigplay Logo"/>
+                <p className="login-hero-info-text1">
+                  Find concerts in your area <br />
+                  based on the songs you <br />
+                  listen to most.</p>
+                <p className="login-hero-info-text2">
+                  And support artists you love
+                </p>
+                <div className="login-hero-info-link">
                   <a href="/login" dangerouslySetInnerHTML={{__html: loginSVG}}></a>                
-                </span>
+                </div>
+                <div className="login-hero-demo">
+                  If you want to demo this app, login with the following credentials:
+                  <div>
+                    Username: gigplaydemo
+                  </div>
+                  <div>
+                    Password: gigplay
+                  </div>
+                </div>
               </div>
             </div>
           </div>
